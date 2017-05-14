9ca9407be5a486cc3521b7b81442fc67b2f76f01
tamura <tamura2004@gmail.com>
2017-05-14 23:58:59 +0900

    fire

--- lib/benchmark/ab.rb
+++ lib/benchmark/ab.rb
@@ -2,11 +2,12 @@ type = "railsapi"
 # type = "static"
 # httpd = "nginx"
 httpd = "puma"
+dbms = "postgres"
 host = "pi3"
 port = "3000"
 url = "pcs"
 
-filename = "#{type}_#{httpd}_#{host}_c100_n300.txt"
+filename = "#{type}_#{host}_#{httpd}_#{dbms}_c100_n300.txt"
 open(filename, "w") do |fh|
   fh.puts `ab -c 100 -n 300 http://#{host}:#{port}/#{url}`
 end
